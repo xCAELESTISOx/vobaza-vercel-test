a87dde860fdcb4d27d454e8aace2eca8518a0d8e
Охико <ohiko@MacBook-Pro-Ohiko.local>
2021-11-24 14:55:42 +0300

    Страница успешного заказа

--- pages/checkout/complete.tsx
+++ pages/checkout/complete.tsx
@@ -1,12 +1,43 @@
-import styles from '../../styles/Cart.module.scss';
+import Link from 'next/link';
+
+import styles from '../../styles/CheckoutComplete.module.scss';
+
+import { Button } from '@nebo-team/vobaza.ui.button';
+import { Icon } from '@nebo-team/vobaza.ui.icon';
+import Breadcrumbs, {
+  BreadcrumbType,
+} from '../../components/Layout/Breadcrumbs';
+import { useRouter } from 'next/router';
+
+const breadcrumbs: BreadcrumbType[] = [
+  {
+    title: 'Ваш заказ принят',
+    href: '/checkout/complete',
+  },
+];
 
 export default function Cart() {
+  const router = useRouter();
+  const { query } = router;
+
   return (
     <div>
+      <Breadcrumbs breadcrumbs={breadcrumbs} />
       <div className="container">
-        <div className={styles.cartContainer}>
-          <h2 className={styles.cartTitle}>Оформление заказа</h2>
-          <div className={styles.cartContent}></div>
+        <div className={styles.checkoutComplete}>
+          <div className={styles.checkoutCompleteIcon}>
+            <Icon name="SmallLogo" />
+          </div>
+          <h1 className={styles.checkoutCompleteTitle}>Спасибо за заказ!</h1>
+          <div className={styles.checkoutCompleteText}>
+            Ваш заказ &#8470;&nbsp;{query.order_id || 0} успешно оформлен. Все
+            данные отправленны на&nbsp;вашу почту и&nbsp;в&nbsp;смс
+          </div>
+          <Link href="/">
+            <a className={styles.checkoutCompleteButton}>
+              <Button text="Вернуться на главную" size="big" isFullScreen />
+            </a>
+          </Link>
         </div>
       </div>
     </div>
