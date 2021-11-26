57d2aad221217bb7f84d33d93c3a70c7e4784cb7
Охико <ohiko@MacBook-Pro-Ohiko.local>
2021-11-26 16:03:15 +0300

    Страница Контакты

---
+++ pages/kontakty.tsx
@@ -0,0 +1,148 @@
+import Link from 'next/link';
+
+import styles from '../styles/Contacts.module.scss';
+
+import Breadcrumbs, { BreadcrumbType } from '../components/Layout/Breadcrumbs';
+import StaticPagesMenu from '../components/StaticPages/Menu';
+
+import VkIcon from '../components/Layout/Footer/Credentials/VkIcon';
+import InstaIcon from '../components/Layout/Footer/Credentials/InstaIcon';
+import FbIcon from '../components/Layout/Footer/Credentials/FbIcon';
+import { Icon } from '@nebo-team/vobaza.ui.icon';
+
+const breadcrumbs: BreadcrumbType[] = [
+  {
+    title: 'Контакты',
+    href: '/kontakty',
+  },
+];
+
+export default function Contacts() {
+  return (
+    <div>
+      <Breadcrumbs breadcrumbs={breadcrumbs} />
+      <div className={`${styles.staticPage} container`}>
+        <StaticPagesMenu />
+        <div
+          className={`${styles.staticPageContent} ${styles.contactsContent}`}
+        >
+          <div>
+            <h1 className={styles.staticPageTitle}>Контакты</h1>
+            <div className={styles.staticPageInfo}>
+              <a href="tel:+74951725818" className={styles.staticPageSubTitle}>
+                +7(495) 172-58-18
+              </a>
+              <div className={`${styles.staticPageText} ${styles.gray}`}>
+                Звонок бесплатный
+              </div>
+              <div className={styles.staticPageText}>
+                Специалисты клиентской поддержки принимают звонки с&nbsp;9:00
+                до&nbsp;21:00 без выходных
+              </div>
+            </div>
+            <div className={styles.staticPageInfo}>
+              <div className={`${styles.staticPageText} ${styles.bold}`}>
+                Склад
+              </div>
+              <div className={styles.staticPageText}>
+                143006&nbsp;Россия, Московская область, Одинцово, Внуковская
+                улица, 9
+              </div>
+            </div>
+            <div className={styles.staticPageInfo}>
+              <div className={`${styles.staticPageText} ${styles.bold}`}>
+                Главный офис
+              </div>
+              <div className={styles.staticPageText}>
+                Россия, Московская область, Одинцово, Луговая улица, 10
+              </div>
+            </div>
+            <div className={styles.staticPageInfo}>
+              <div className={`${styles.staticPageText} ${styles.bold}`}>
+                Реквизиты
+              </div>
+              <div className={styles.staticPageText}>
+                Наименование организации: ООО &laquo;Вобаза&raquo; ИНН/КПП
+                6167198895/616701001&nbsp;ОГРН 1206100021863 Юридический адрес:
+                344111, Ростовская обл., г. Ростов-на-Дону, ул. 50-летия
+                Октября, д. 33/1, офис&nbsp;1
+              </div>
+            </div>
+            <div className={styles.staticPageInfo}>
+              <div className={`${styles.staticPageText} ${styles.bold}`}>
+                Почта для партнеров
+              </div>
+              <div>
+                <a
+                  href="mailto:partners@vobaza.ru"
+                  className={`${styles.staticPageText} ${styles.link}`}
+                >
+                  partners@vobaza.ru
+                </a>
+              </div>
+            </div>
+            <div className={styles.staticPageInfo}>
+              <div className={`${styles.staticPageText} ${styles.bold}`}>
+                Подпишитесь в&nbsp;социальных сетях
+              </div>
+              <div className={styles.contactsSocial}>
+                <a
+                  href="https://www.facebook.com/vobaza.ru"
+                  target="_blank"
+                  className={styles.contactsSocialItem}
+                  rel="noreferrer"
+                >
+                  <FbIcon />
+                </a>
+                <a
+                  href="https://vk.com/vobaza_official"
+                  target="_blank"
+                  className={styles.contactsSocialItem}
+                  rel="noreferrer"
+                >
+                  <VkIcon />
+                </a>
+                <a
+                  href="https://www.instagram.com/vobaza.ru/"
+                  target="_blank"
+                  className={styles.contactsSocialItem}
+                  rel="noreferrer"
+                >
+                  <InstaIcon />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className={styles.staticPageInfo}>
+              <div className={`${styles.staticPageText} ${styles.bold}`}>
+                Обратная связь
+              </div>
+              <div className={styles.staticPageText}>
+                Если у&nbsp;вас возник вопрос&nbsp;&mdash; напишите нам
+              </div>
+              <Link href="/kontaktnaya-forma">
+                <a className={styles.contactsButton}>
+                  Написать <Icon name="ArrowRight" />
+                </a>
+              </Link>
+            </div>
+            <div className={styles.staticPageInfo}>
+              <div className={`${styles.staticPageText} ${styles.bold}`}>
+                Поставщикам
+              </div>
+              <div className={styles.staticPageText}>
+                Узнайте подробные условия для сотрудничества
+              </div>
+              <Link href="/postavschikam-partneram">
+                <a className={styles.contactsButton}>
+                  Узнать подробности <Icon name="ArrowRight" />
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
