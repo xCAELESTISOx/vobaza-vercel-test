6b23862248bf5c2e1365a73487bef984c48866a5
eduard.pavlov <eduard.pavlov@immelman.ru>
2021-11-29 10:06:17 +0300

    Добавлен HOC модалки

---
+++ src/hoc/withModal.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+
+import styles from "./withModal.module.scss";
+import { Icon } from "@nebo-team/vobaza.ui.icon";
+
+export interface IModalLayout {
+  onClose(): void;
+}
+
+export default function ModalLayout({
+  children,
+  onClose,
+}: IModalLayout & { children: React.ReactNode }) {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  const modalClick = (e: any) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div className={styles.modalLayout} onClick={onClose}>
+      <div className={styles.modal} onClick={modalClick}>
+        <div className={styles.modalClose} onClick={onClose}>
+          <Icon name="Cross" />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
