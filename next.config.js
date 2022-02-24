0be4077d8cf9aad3279dfa3f0d7dadf5d1be675f
Daria (DarkOhiko) Kolotushkina <daria.kolotushkina@immelman.ru>
2022-02-24 10:07:23 +0300

    Добавлен хост с картинками для второго тестового

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['vobaza-dev-public.s3.us-west-000.backblazeb2.com'],
+    domains: ['vobaza-dev-public.s3.us-west-000.backblazeb2.com', 'vobaza-vobaza-demo-public.s3.us-west-000.backblazeb2.com'],
   },
 };
