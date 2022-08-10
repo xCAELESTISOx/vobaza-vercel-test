0a0da9a71fd01c9343b05fb2dfcd92f64078c880
Dmitry Demidov <sloory@mail.ru>
2022-08-10 15:15:35 +0300

    * add vobaza ci files

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+odule.exports = {
+    apps: [
+      {
+        name: 'front',
+        exec_mode: 'cluster',
+        instances: '2', // Or a number of instances
+        script: 'yarn',
+        args: 'start',
+        autorestart: true,
+        max_memory_restart: '1G',
+        env: {
+          NODE_ENV: 'production',
+          PORT: '3000'
+        }
+      }
+    ],
+  deploy : {
+      production : {
+         "user" : "www-data",
+         "host" : ["10.10.0.19"],
+         "ref"  : "origin/develop",
+         "repo" : "git@git.dc-vbz.ru:mnikulin/vb-market-frontend.git",
+         "path" : "/srv/front",
+         "post-deploy" : "npm install && yarn && yarn build && pm2 reload ecosystem.config.js --env production && pm2 save"
+      }
+    }
+  };
