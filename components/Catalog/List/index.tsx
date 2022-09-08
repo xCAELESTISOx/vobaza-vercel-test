f8502084fc9c4aa0d30c22eb50d6541c85b777d0
andrey.brazhnikov <andrey.brazhnikov@noreply.nebo.ossbuild.ru>
2022-09-08 18:46:25 +0300

    fix: Ссылки на категориях Co-Authored-By: andrey.brazhnikov <andrey.brazhnikov@noreply.nebo.ossbuild.ru> Co-Committed-By: andrey.brazhnikov <andrey.brazhnikov@noreply.nebo.ossbuild.ru>

--- components/Catalog/List/index.tsx
+++ components/Catalog/List/index.tsx
@@ -2,33 +2,50 @@ import { FC, useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import styles from './styles.module.scss';
 import type { ICategory } from '../../../src/models/ICategory';
-import PlaceholderImage from 'assets/images/placeholder_small.png';
 import { getImageVariantProps } from 'assets/utils/images';
 
 import { Icon } from '@nebo-team/vobaza.ui.icon/dist';
+
+import PlaceholderImage from 'assets/images/placeholder_small.png';
+
+import styles from './styles.module.scss';
 
 type Props = {
   list: ICategory[];
 };
 
 const getHref = (category: ICategory) => {
-  let href = '';
-  category.ancestors?.forEach((ancestor) => {
-    href += `/${ancestor.slug}`;
-  });
-  href += `/${category.slug}`;
-  return href;
+  const newAncestors = category.ancestors?.reduce(
+    (acc, ancestor, i) => {
+      if (ancestor.parent_id) {
+        const index = acc.findIndex((item) => item.id === ancestor.parent_id);
+
+        const newAcc = [...acc];
+
+        if (index > -1 && !newAcc[i].slug.includes(newAcc[index].slug + '/')) {
+          newAcc[i].slug = newAcc[index].slug + '/' + newAcc[i].slug + '/' + category.slug;
+        }
+
+        return newAcc;
+      } else {
+        return acc;
+      }
+    },
+    [...category.ancestors]
+  );
+
+  if (!newAncestors) {
+    return category.slug;
+  } else {
+    return newAncestors?.length > 1
+      ? newAncestors.filter((item) => item.parent_id)[0].slug
+      : newAncestors[0].slug + '/' + category.slug;
+  }
 };
 
-// Анцесторы приходят в обратном порядке, поэтому нам нужно их реверсить
 const normalizeCategories = (categories: ICategory[]) => {
-  const newCategories = categories.map((category) => {
-    const newAncestors = category.ancestors.slice().reverse();
-    return { ...category, ancestors: newAncestors };
-  });
-
+  const newCategories = categories.map((item) => ({ ...item, slug: `/${getHref(item)}` }));
   return newCategories;
 };
 
@@ -42,7 +59,7 @@ const CatalogList: FC<Props> = ({ list }) => {
   return (
     <nav className={styles.catalog}>
       {categoryiesList.map((category) => (
-        <Link key={category.id} href={getHref(category)}>
+        <Link key={category.id} href={category.slug}>
           <a className={styles.category}>
             <div className={styles.categoryImage}>
               {category.image ? (
