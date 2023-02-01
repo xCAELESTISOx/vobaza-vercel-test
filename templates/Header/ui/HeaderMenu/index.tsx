75ec3dcd4cbb7f7a9e37e321c9a2893cb19c8d52
andrey.brazhnikov <andrey.brazhnikov@noreply.nebo.ossbuild.ru>
2023-02-01 18:22:07 +0300

    - fix: Задержка открытия меню. (#201) Co-Authored-By: andrey.brazhnikov <andrey.brazhnikov@noreply.nebo.ossbuild.ru> Co-Committed-By: andrey.brazhnikov <andrey.brazhnikov@noreply.nebo.ossbuild.ru>

--- templates/Header/ui/HeaderMenu/index.tsx
+++ templates/Header/ui/HeaderMenu/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, MouseEvent } from 'react';
+import React, { FC, useState, MouseEvent, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
@@ -18,6 +18,9 @@ type Props = {
 export const HeaderMenu: FC<Props> = ({ mainMenu, sideMenu }) => {
   const [currentMenu, setCurrentMenu] = useState<IMenuItem | IMenuItem[]>(null);
   const [withRoot, setWithRoot] = useState(false);
+  const [itemEvent, setItemEvent] = useState<MouseEvent<HTMLAnchorElement> | null>(null);
+
+  let timer: NodeJS.Timeout;
 
   const router = useRouter();
   const isExpress = router.asPath.includes('/ekspress-dostavka');
@@ -30,7 +33,7 @@ export const HeaderMenu: FC<Props> = ({ mainMenu, sideMenu }) => {
 
   const openFullMenu = (e: any) => {
     setWithRoot(false);
-    if (mainMenu[e.target.dataset.index]) {
+    if (e && mainMenu[e.target.dataset.index]) {
       setCurrentMenu(mainMenu[e.target.dataset.index]);
     } else {
       setCurrentMenu(null);
@@ -41,6 +44,18 @@ export const HeaderMenu: FC<Props> = ({ mainMenu, sideMenu }) => {
     setWithRoot(false);
     setCurrentMenu(null);
   };
+
+  const handleMouseEnter = (e: MouseEvent<HTMLAnchorElement>) => {
+    timer = setTimeout(() => {
+      setItemEvent(e);
+    }, 500);
+  };
+
+  useEffect(() => {
+    if (itemEvent) {
+      openMenuAll();
+    }
+  }, [itemEvent]);
 
   const isSideMenu = currentMenu instanceof Array;
 
@@ -53,7 +68,8 @@ export const HeaderMenu: FC<Props> = ({ mainMenu, sideMenu }) => {
         <Link href={katalogLink}>
           <a
             className={`${styles.headerCategory} ${styles.headerCategoryAll} ${withRoot ? styles.active : ''}`}
-            onMouseEnter={openMenuAll}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={() => clearTimeout(timer)}
             onClick={() => {
               closeMenu();
             }}
@@ -83,13 +99,31 @@ const HeaderMenuItem = ({ index, item, openFullMenu, closeMenu }: MainMenuItemPr
   const isExpress = router.asPath.includes('/ekspress-dostavka');
   const link = getLinkFromMenuItem(item, isExpress);
 
+  const [itemEvent, setItemEvent] = useState<MouseEvent<HTMLAnchorElement> | null>(null);
+
+  let timer: NodeJS.Timeout;
+
+  const handleMouseEnter = (e: MouseEvent<HTMLAnchorElement>) => {
+    timer = setTimeout(() => {
+      setItemEvent(e);
+    }, 500);
+  };
+
+  useEffect(() => {
+    if (itemEvent) {
+      openFullMenu(itemEvent);
+    }
+  }, [itemEvent]);
+
   return (
     <Link href={link}>
       <a
         className={` ${styles.headerCategory} ${router.asPath.includes(link) ? styles.active : ''}`}
         data-index={index}
-        onMouseEnter={openFullMenu}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={() => clearTimeout(timer)}
         onClick={() => {
+          setItemEvent(null);
           closeMenu();
         }}
       >
